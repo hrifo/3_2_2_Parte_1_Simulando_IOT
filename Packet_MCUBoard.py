8f7a5fe8d76a8bb51ab2fc82619f69042098de71
Hector Rifo <54448892+hrifo@users.noreply.github.com>
2021-11-27 10:58:49 -0300

    Update Packet_MCUBoard.py

--- Packet_MCUBoard.py
+++ Packet_MCUBoard.py
@@ -1,22 +1,21 @@
 from gpio import *
-#Asignar puertos digitales en placa mcu
-luz = 0
-#puerta=1
-sirena=2
-#ventana=3
-regadera=4
-#garage=5
-while True:
-	puerta = customRead(1)
-	ventana = customRead(3)
-	garage = customRead(5)
-	if garage == "1" or puerta == "1,0" or ventana == "1":
-		customWrite(luz,"2")
-		customWrite(sirena,"1")
-		customWrite(regadera,"1")
-		
 
-	else:
-		customWrite(luz,"0")
-		customWrite(sirena,"0")
-		customWrite(regadera,"0")
+# Asignar puertos digitales en placa mcu
+luz = 0
+# puerta=1
+sirena = 2
+# ventana=3
+regadera = 4
+# garage=5
+while True:
+    puerta = customRead(1)
+    ventana = customRead(3)
+    garage = customRead(5)
+    if garage == "1" or puerta == "1,0" or ventana == "1":
+        customWrite(luz, "2")
+        customWrite(sirena, "1")
+        customWrite(regadera, "1")
+    else:
+        customWrite(luz, "0")
+        customWrite(sirena, "0")
+        customWrite(regadera, "0")
